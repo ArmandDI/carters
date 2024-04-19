7ac11f151fb71384c658c1e3f90b67fa2f9003d8
Armand Buys <armand.buys@datainsight.co.nz>
2024-04-19 14:50:07 +1200

    Create GL Sources.sql

---
+++ code/sql/GL Sources.sql
@@ -0,0 +1,17 @@
+
+-- =============================================
+-- gl_sources
+--
+-- Purpose: Stores all GL Sources for GL Transactions for DS2 - GL Transactions Dataset
+-- Created By: AB (20240419)
+
+-- Modified By:
+-- YYYYMMDD (XX): Added columns XXX for DSX - XXX Dataset.
+
+-- =============================================
+
+
+SELECT 
+	 [journalname] AS [gl_source_code]
+	,[name]	AS [gl_source_name]
+FROM ledgerjournalname
